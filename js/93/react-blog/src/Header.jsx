cc7035f8dbc9d76d5e543fdafd308e5a62dc81fa
EtaRosenbaum <etarosenbaum@gmail.com>
2026-01-14 11:42:40 -0500

    react blog done

---
+++ js/93/react-blog/src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <header>
+            <Link to='/' > Developer Blogs</Link>
+
+        </header>
+
+
+    )
+}
