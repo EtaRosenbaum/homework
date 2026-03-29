70693b05db34b081e6f5394123576ae92ff5bdde
EtaRosenbaum <etarosenbaum@gmail.com>
2026-03-29 19:40:52 -0400

    hw 110 done

---
+++ python/110/hw110.py
@@ -0,0 +1,22 @@
+print('Hello world!')
+
+x = 5
+print(type(x))
+print(x)
+
+
+first= 'Donald'
+last = 'Trump'
+age = 80
+
+print(f'{first} {last} is President. He is {age}')
+
+a = 7
+
+print(f"before {a:3d} after")   
+print(f"before {a:>3d} after")
+print(f"before {a:<3d} after")
+print(f"before {a:^3d} after")
+
+b = 1234567
+print(f"Number with commas: {b:,}")
